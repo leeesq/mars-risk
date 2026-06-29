003c759e9863f1a775a6b582707dfc56d06fcb1f
李士强 <lee_sq001@163.com>
2026-06-29 09:13:04 +0800

    v0.0.20

--- src/mars/__init__.py
+++ src/mars/__init__.py
@@ -41,7 +41,7 @@ from .reporting import MarsBinningReport, MarsProfileReport
 from .scoring import MarsScorecard, build_scorecard
 from .utils import logger, set_log_level
 
-__version__ = "0.0.2"
+__version__ = "0.0.20"
 
 _BANNER = rf"""
     __________________________________________________________________________
